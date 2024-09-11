cdcc551d414884953c9904976694e8e35baf8d71
iagosm <iagosousa201486@gmail.com>
2024-09-11 00:38:20 -0300

    stateless, autenticacao por token e laravel sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use App\Models\Episode;
 use App\Models\Series;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,16 +23,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/series', \App\Http\Controllers\Api\SeriesController::class);
+    Route::get('/series/{series}/seasons', function(Series $series) {
+        return $series->seasons;
+    });
+    Route::get('/series/{series}/episodes', function(Series $series) {
+        return $series->episodes;
+    });
+    Route::patch('/episodes/{episode}', function(Episode $episode, Request $request) {
+        $episode->watched = $request->watched;
+        $episode->save();
+        return $episode;
+    });
+});
 
-Route::apiResource('/series', \App\Http\Controllers\Api\SeriesController::class);
-Route::get('/series/{series}/seasons', function(Series $series) {
-    return $series->seasons;
-});
-Route::get('/series/{series}/episodes', function(Series $series) {
-    return $series->episodes;
-});
-Route::patch('/episodes/{episode}', function(Episode $episode, Request $request) {
-    $episode->watched = $request->watched;
-    $episode->save();
-    return $episode;
+Route::post('/login', function(Request $request) {
+    $credentials = $request->only(['email', 'password']);
+    if (Auth::attempt($credentials) === false) {
+        return response()->json("Unauthorized",401);
+    }
+    $user = Auth::user();
+    $token = $user->createToken('token');
+    // $token = $user->createToken('token', 'pode_removerseries');
+    return response()->json($token->plainTextToken);
+    // $user = User::whereEmail($credentials['email'])->first();
+    // if ($user === null || Hash::check($credentials['password'], $user->password) === false) {
+    //     return response()->json("Unauthorized",401);
+    // }
 });
